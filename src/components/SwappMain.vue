1ac7239e8a6f65eaa99c6c892c278ffdcc72f319
Alex Kuznetsov <alex.y.kuznetsov@gmail.com>
2023-02-15 18:05:20 +1100

    card background and layout changes

--- src/components/SwappMain.vue
+++ src/components/SwappMain.vue
@@ -4,33 +4,43 @@
     <div>Deck in</div>
     <div class="swapp-item-mid">The swap</div>
     <div>Deck out</div>
-    <div class=" swapp-item-controls"></div>
+    <div class="swapp-item-controls"></div>
   </div>
 
   <template v-if="isInit">
-    <div class="swapp-item swapp-item-general" v-for="(item, itemKey) in swappList" :key="itemKey">
-      <div>
-        <div class="swapp-input">{{ item.deckIn ? item.deckIn.name : '-' }}</div>
-      </div>
-      <div class="swapp-item-mid">
-        <div class="swapp-item-mid-cover">
-          <div class="swapp-item-image swapp-item-image-left">
-            <img :src="item.cardIn ? item.cardIn.image_uris.normal : require('@/assets/images/card_back.jpg')">
-          </div>
-          <div class="swapp-item-image swapp-item-image-right">
-            <img :src="item.cardOut ? item.cardOut.image_uris.normal : require('@/assets/images/card_back.jpg')">
+    <div class="swapp-item-cover" v-for="(item, itemKey) in swappList" :key="itemKey">
+      <div class="swapp-item swapp-item-general">
+        <div class="swapp-segment">
+          <div class="swapp-segment-bg swapp-segment-bg-left" 
+            v-if="item.deckIn" 
+            :style="{ backgroundImage: `url('${item.deckIn.image_uris.art_crop})` }"
+          ></div>
+          <div class="swapp-input">{{ item.deckIn ? item.deckIn.name : '-' }}</div>
+        </div>
+        <div class="swapp-segment swapp-item-mid">
+          <div class="swapp-item-mid-cover">
+            <div class="swapp-item-image swapp-item-image-left">
+              <img :src="item.cardIn ? item.cardIn.image_uris.normal : require('@/assets/images/card_back.jpg')">
+            </div>
+            <div class="swapp-item-image swapp-item-image-right">
+              <img :src="item.cardOut ? item.cardOut.image_uris.normal : require('@/assets/images/card_back.jpg')">
+            </div>
           </div>
         </div>
-      </div>
-      <div>
-        <div class="swapp-input">{{ item.deckOut ? item.deckIn.name : '-' }}</div>
-      </div>
-      <div class=" swapp-item-controls">
+        <div class="swapp-segment">
+          <div class="swapp-segment-bg swapp-segment-bg-right" 
+            v-if="item.deckOut" 
+            :style="{ backgroundImage: `url('${item.deckOut.image_uris.art_crop})` }"
+          ></div>
+          <div class="swapp-input">{{ item.deckOut ? item.deckOut.name : '-' }}</div>
+        </div>
+      </div> 
+      <div class="swapp-item-controls">
         <button class="swapp-item-button" @click="openEditModal(item.id)">Edit</button>
         <button class="swapp-item-button" @click="deleteSwappItem(item.id)">Del</button>
         <button class="swapp-item-button">Top</button>
       </div>
-    </div> 
+    </div>
   </template>
 
 </template>
@@ -91,23 +101,79 @@ export default {
 </script>
 
 <stype lang="less" scoped>
+  .swapp-item-cover {
+    display: grid;
+    grid-template-columns: auto 40px;
+  }
+
   .swapp-item {
     border-radius: 6px;
-    padding: 10px;
+    overflow: hidden;
     display: grid;
-    grid-template-columns: 20% auto 20% 10%;
-    gap: 10px;
+    grid-template-columns: 30% auto 30%;
+    gap: 0 10px;
     background-color: var(--color-bg);
+  }
+
+  .swapp-segment {
+    padding: 10px 0;
+    background-repeat: no-repeat;
+    position: relative;
+
+    &:first-child {
+      padding-left: 10px;
+    }
+
+    &:last-child {
+      padding-right: 10px;
+    }
+  }
+
+  .swapp-segment-bg {
+    height: 100%;
+    width: 100%;
+    left: 0;
+    top: 0;
+    position: absolute;
+    z-index: 0;
+    background-repeat: no-repeat;
+    background-size: cover;
+    opacity: 0.5;
+    background-position: 50% 50%;
+
+    &.swapp-segment-bg-left {
+      &::after {
+        position: absolute;
+        content: '';
+        height: 100%;
+        width: 50%;
+        right: 0;
+        top: 0;
+        background: linear-gradient(to right, transparent, var(--color-bg));
+      }
+    }
+
+    &.swapp-segment-bg-right {
+      &::before {
+        position: absolute;
+        content: '';
+        height: 100%;
+        width: 50%;
+        left: 0;
+        top: 0;
+        background: linear-gradient(to left, transparent, var(--color-bg));
+      }
+    }
   }
 
   .swapp-item-head {
     margin-bottom: 10px;
     text-align: center;
+    padding: 4px 0;
+    grid-template-columns: 30% auto 30% 40px;
   }
 
   .swapp-item-general {
-    height: 120px;
-
     & + & {
       margin-top: 10px;
     }
@@ -121,6 +187,7 @@ export default {
     font-size: 22px;
     border: none;
     border-bottom: 1px solid var(--color-main);
+    position: relative;
 
     &:focus {
       outline: none;
@@ -182,17 +249,20 @@ export default {
   }
 
   .swapp-item-button {
+    border: 2px solid var(--color-bg);
+    color: var(--color-bg);
     margin: 0;
+    padding: 0;
     height: 36px;
     width: 36px;
     cursor: pointer;
-    border: 1px solid var(--color-main);
-    border-radius: 4px;
+    border-radius: 6px;
+    font-weight: 600;
     transition: all var(--main-transition);
 
     &:hover {
-      color: var(--color-secondary);
-      border: 1px solid var(--color-secondary);
+      color: var(--color-white);
+      background-color: var(--color-bg);
       transition: all var(--main-transition);
     }
   }
